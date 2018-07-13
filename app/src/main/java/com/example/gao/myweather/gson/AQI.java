9177c3d6582ea1a5bca93ea1439177eb5c7b1225
Gao <ddhhss4569630@163.com>
2018-07-13 16:05:29 +0800

    完成遍历省市县三级列表功能

---
+++ app/src/main/java/com/example/gao/myweather/gson/AQI.java
@@ -0,0 +1,8 @@
+package com.example.gao.myweather.gson;
+
+/**
+ * Created by GAO on 2018/7/13.
+ */
+
+public class AQI {
+}
--- app/src/main/java/com/example/gao/myweather/gson/Basic.java
+++ app/src/main/java/com/example/gao/myweather/gson/Basic.java
@@ -1,8 +1,19 @@
 package com.example.gao.myweather.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by GAO on 2018/7/13.
  */
 
 public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+    public Updata updata;
+    public class Updata{
+        @SerializedName("loc")
+        public String updataTime;
+    }
 }
